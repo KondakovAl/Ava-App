baaccc7d92138169742a3d373ea96e3d96e3f462
KondakovAl <99503086+KondakovAl@users.noreply.github.com>
2022-04-16 16:58:30 +0300

    day 3 of coding
    Form + faq (without js)

--- js/script.js
+++ js/script.js
@@ -7,3 +7,17 @@ burger.addEventListener('click', () => {
     menuMobile.classList.toggle('_active');
     document.body.classList.toggle('_lock');
 })
+
+//faq
+// let plus = document.querySelectorAll('.faq__icon').forEach(item) =>
+//     item.addEventListener('click', () => {
+//         if (parent.classList.contains(faq__answer-container)
+//     }
+
+let plus = document.querySelector('.faq__icon')
+let answer = document.querySelector('.faq__answer-container');
+
+plus.addEventListener('click', () => {
+    plus.classList.toggle('_open');
+    answer.classList.toggle('_open');
+})
