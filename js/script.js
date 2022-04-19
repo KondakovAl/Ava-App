160ce4edb3cae16b3249953ea7013cc561b4576e
KondakovAl <99503086+KondakovAl@users.noreply.github.com>
2022-04-19 19:34:49 +0300

    5 day of coding
    non-adaptive
    non-dynamic

--- js/script.js
+++ js/script.js
@@ -21,3 +21,31 @@ plus.addEventListener('click', () => {
     plus.classList.toggle('_open');
     answer.classList.toggle('_open');
 })
+
+
+//Slider//
+
+const swiper = new Swiper('.swiper', {
+    // // Optional parameters
+    // direction: 'vertical',
+    // loop: true,
+  
+    
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    
+    // // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      
+    },
+
+    // // And if we need scrollbar
+    // scrollbar: {
+    //   el: '.swiper-scrollbar',
+    // },
+  });
