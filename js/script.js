8cb6f97164bf560a68f1281eb37db78be0ea7495
KondakovAl <99503086+KondakovAl@users.noreply.github.com>
2022-04-26 20:57:36 +0300

    The End
    Bug to fix:
    1. Burger
    2. Slider
    3. Breakpoins (width of blocks: form and price)

--- js/script.js
+++ js/script.js
@@ -1,51 +1,111 @@
+// BURGER //
+
 let burger = document.querySelector('.burger');
 let menuMobile = document.querySelector('.menu_mobile');
 
-
 burger.addEventListener('click', () => {
+    document.body.classList.toggle('_lock');
     burger.classList.toggle('_active');
     menuMobile.classList.toggle('_active');
-    document.body.classList.toggle('_lock');
-})
-
-//faq
-// let plus = document.querySelectorAll('.faq__icon').forEach(item) =>
-//     item.addEventListener('click', () => {
-//         if (parent.classList.contains(faq__answer-container)
-//     }
-
-let plus = document.querySelector('.faq__icon')
-let answer = document.querySelector('.faq__answer-container');
-
-plus.addEventListener('click', () => {
-    plus.classList.toggle('_open');
-    answer.classList.toggle('_open');
-})
-
-
-//Slider//
-
-const swiper = new Swiper('.swiper', {
-    // // Optional parameters
-    // direction: 'vertical',
-    // loop: true,
+  })
   
+  if (burger.classList.contains('_active')) {
+      document.body.classList.remove('_lock');
+      burger.classList.remove('_active');
+      menuMobile.classList.remove('_active');
+  }
+
+
+//FAQ//
+
+    document.querySelectorAll('.faq__question').forEach((item) =>
+    item.addEventListener('click', () => {
+      const parent = item.parentNode;
+
+      if (parent.classList.contains('faq__list-item--active')) {
+        parent.classList.remove('faq__list-item--active');
+      } else {
+        document.querySelectorAll('.faq__list-item').forEach((child) =>
+        child.classList.remove('faq__list-item--active'))
+        parent.classList.add('faq__list-item--active');
+      }
+    })
+    )
+
+// SCROLL BUTTON //
+
+    let upButton = document.querySelector('.up-button');
+
+    window.onscroll = function () {
+        if (window.pageYOffset > 400) {
+          upButton.classList.add('--shown');
+        } else {
+          upButton.classList.remove('--shown');
+        }
+      
+      };
+
+      upButton.onclick = function () {
+        window.scrollTo(0, 0);
+      };
+  
+  //SLIDER//
+  
+const swiper = new Swiper('.swiper', {
     
-    // Navigation arrows
+    loop: true,
+  
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
     },
     
-    // // If we need pagination
     pagination: {
       el: '.swiper-pagination',
       clickable: true,
-      
     },
 
-    // // And if we need scrollbar
-    // scrollbar: {
-    //   el: '.swiper-scrollbar',
+    grabCursor: true,
+
+    hashNavigation: {
+        watchState: true,
+    },
+
+    
+    keyboard: {
+        enabled: true,
+        onlyInViewport: true,
+        pageUpDown: true,
+    },
+
+    slideToClickedSlide: true,
+
+    slidesPerView: 'auto',
+
+    spaceBetween: 20,
+
+    // breakpoints: {
+      
+    //   786: {
+    //     spaceBetween: 20
+    //   }
+  
     // },
+    
+    freeMode: true,
+
+    autoplay: {
+      delay: 2000,
+        // stopOnLastSlide: true,
+        disableOnInteraction: false,
+    },
+    speed: 800,
   });
+  
+  //ANIMATION//
+
+  AOS.init({
+    offset: 150,
+    once: true,
+  });
+  
